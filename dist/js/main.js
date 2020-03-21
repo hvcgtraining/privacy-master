a3dd41438e5c3e69bbe7a41e3e3c3fdf4d8304b6
lethanh070388 <thanhtruong070388@gmail.com>
2020-03-21 11:28:33 +0700

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,7 @@
 "use strict";
+
+$("#btn-backtotop").click(function () {
+  $('body,html').animate({
+    scrollTop: 0
+  }, 700);
+});
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+$("#btn-backtotop").click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    },700)
+})
