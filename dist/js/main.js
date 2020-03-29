629197e5e670bccc1647699ca5a15dbf6441b80c
lethanh070388 <thanhtruong070388@gmail.com>
2020-03-29 15:48:39 +0700

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,3 +5,11 @@ $("#btn-backtotop").click(function () {
     scrollTop: 0
   }, 700);
 });
+$('#btn-sub-popup').click(function () {
+  $('#bg-form-air').addClass('d-none').removeClass('d-block');
+  $('#submit-success').addClass('d-block').removeClass('d-none');
+});
+$('#btn-ok').click(function () {
+  $('#bg-form-air').addClass('d-block').removeClass('d-none');
+  $('#submit-success').addClass('d-none').removeClass('d-block');
+});
